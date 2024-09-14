2a5a0d24c790687b27cf09b3dde2cf9cc06fb4ae
LOVE-AMOR <loveamorbalaqui2@gmail.com>
2024-09-14 15:04:50 -0800

    Initial commit

---
+++ dataconvertion.js
@@ -0,0 +1,32 @@
+// Convert a String to a Number
+let price = '200';
+
+// Using parseInt()
+let priceParsed = parseInt(price);
+console.log('Using parseInt():', priceParsed);
+
+// Using Number()
+let priceNumber = Number(price);
+console.log('Using Number():', priceNumber);
+
+// Using + operator
+let pricePlus = +price;
+console.log('Using + operator:', pricePlus);
+
+// Convert a Number to a String
+let distance = 150;
+
+// Using .toString()
+let distanceToString = distance.toString();
+console.log('Using .toString():', distanceToString);
+
+// Using String()
+let distanceString = String(distance);
+console.log('Using String():', distanceString);
+
+// Convert a String to a Decimal Number
+let weight = '72.5';
+
+// Using parseFloat()
+let weightDecimal = parseFloat(weight);
+console.log('Using parseFloat():', weightDecimal);
